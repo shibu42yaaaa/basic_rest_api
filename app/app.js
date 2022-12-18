eff06eb5b6b8480d59743f061911aba5a4e6108d
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-19 01:43:29 +0900

    update deleteの記述を追加

--- app/app.js
+++ app/app.js
@@ -2,9 +2,16 @@ const expres = require('express')
 const app = express()
 const sqlite3 = require('sqlite3')
 const path = require('path')
+const bodyParser = require('body-parser')
+const { resolveMx } = require('dns')
 
 const dbPath = "app/db/database.sqlite3"
 
+// リクエストのbodyをパースする設定
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+//publicディレクトリを静的ファイル群のルートディレクトリとして設定
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Get all users
@@ -25,7 +32,7 @@ app.get('/api/v1/users/:id', (req, res) => {
   const db = new sqlite3.Database(dbPath)
   const id = req.params.id
 
-  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, rows) => {
+  db.get(`SELECT * FROM users WHERE id=${id}`, (err, row) => {
     res.json(row)
   })
 
@@ -44,6 +51,81 @@ app.get('/api/v1/search', (req, res) => {
 
   db.close()
 })
+
+
+// DBクエリ実行用の関数の作成
+const run = async (sql, db, res, message) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, (err) => {
+      if (err) {
+        res.status(500).send(err)
+        return reject();
+      } else {
+        res.json({message: message})
+        return resolve();
+      }
+    });
+  })
+}
+
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+  const db = new sqlite3.Database(dbPath)
+
+  const name = req.body.name
+  const profile = req.body.profile ? req.body.profile : ""
+  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+  await run(
+     'INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")',
+     db,
+     res,
+       "新規ユーザを作成しました"
+    )
+  db.close
+})
+
+
+// Update user data
+app.put('/api/v1/users/:id', async (req, res) => {
+  const db = new sqlite3.Database(dbPath)
+  //IDの取得
+  const id = req.params.id
+
+  // 現在のユーザ情報を取得する
+  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+    const name = req.body.name ? req.body.name : row.name
+    const profile = req.body.profile ? req.body.profile : row.profile
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+
+    await run(
+      'UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHEREb id=${id}',
+      db,
+      res,
+      "ユーザ情報を更新しました"
+    )
+  })
+
+  db.close()
+})
+
+
+// Delete user data
+app.delete('/api/v1/users/:id', async (req, res) => {
+  const db = new sqlite3.Database(dbPath)
+  //IDの取得
+  const id = req.params.id
+
+  await run(
+    'DELETE FROM users WHERE id=${id}',
+    db,
+    res,
+      "ユーザ情報を削除しました"
+    )
+
+  db.close()
+})
+
 
 
 
