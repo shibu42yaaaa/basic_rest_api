16549df0a86c6aa01cad80a9f4fcdf15e593e7fb
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-20 00:11:31 +0900

    ユーザ編集と削除を追加

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -20,11 +20,13 @@ const usrsModule = (() => {
                         <td>${user.date_of_birth}</td>
                         <td>${user.created_at}</td>
                         <td>${user.updated_at}</td>
+                        <td><a href="edit.html?uid=$(user.uid)">編集</a></td>
                       </tr>`
         document.getElementById('users-list').insertAdjacentElementHTML('beforeend', body)
       }
     },
     // POSTの値を取得する
+    // ユーザ作成
     createUser: async () => {
       const name = document.getElementById("name").value
       const profile = document.getElementById("profile").value
@@ -47,6 +49,57 @@ const usrsModule = (() => {
 
       alert(resJson.message)
       window.location.href = "/"
+    },
+    // ユーザ編集
+    setExistingValue: async (uid) => {
+      const res = await fetch(BASE_URL + "/" + uid)
+      const resJson = await res.json()
+
+      document.getElementById('name').value = resJson.name
+      document.getElementById('profile').value = resJson.profile
+      document.getElementById('date-of-birth').value = resJson.date_of_birth
+    },
+    saveUser: async (uid) => {
+      const name = document.getElementById("name").value
+      const profile = document.getElementById("profile").value
+      const dateOfBirth = document.getElementById("date-of-birth").value
+      
+      // リクエストのbody
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateOfBirth
+      }
+
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+
+      const resJson = await res.json
+
+      alert(resJson.message)
+      window.location.href = "/"
+    },
+    // ユーザ削除
+    deleteUser: async (uid) =>{
+      const ret = window.confirm('このユーザを削除しますか？')
+
+      if (!ret) {
+        return false
+      } else {
+        const res = await fetch(BASE_URL + "/" + uid, {
+          method: "DELETE",
+          headers: headers,
+        })
+
+        const resJson = await res.json()
+        
+        alert(resJson.message)
+        window.location.href = '/'
+    
+      }
     }
   }
 })()
